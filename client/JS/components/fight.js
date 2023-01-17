5483c94d040fcb68264125010e44abdf5efea2e1
mark91 <mark.mekhayl@gmail.com>
2023-01-17 13:55:08 +1100

    test

---
+++ client/JS/components/fight.js
@@ -0,0 +1,17 @@
+function renderFight() {
+    document.getElementById('#page') = ` 
+    <div class="fight-box">
+        <div class="player-character">
+            <img src=`` alt="">
+        </div>
+        <div class="dice">
+            <img src="" alt="">
+        </div>
+        <div class="monster">
+            <img src="https://i.imgur.com/9j6T4H5.png" alt="">
+        </div>
+
+    </div>
+    `;
+}
+
