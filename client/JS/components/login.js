589151a1d49374d115e3f6d88410e70a461534b8
sakalmon <sakalathmon@gmail.com>
2023-01-17 10:55:22 +1000

    log-in-feature

--- client/JS/components/login.js
+++ client/JS/components/login.js
@@ -1,53 +1,53 @@
 // Show the log-in screen
 function renderLogin() {
-  document.querySelector('#page').innerHTML = `
-    <section class='log-in'>
-      <form onSubmit="login(event)">
-        <h2>Log In:</h2>
-        <fieldset>
-          <label for="">Email: </label>
-          <input type="text" name="email">
-        </fieldset>
-        <fieldset>
-          <label for="">Password: </label>
-          <input type="password" name="password">
-        </fieldset>
-        <button>Login</button>
-      </form>
-    </section>
-  `;
+    document.querySelector('#page').innerHTML = `
+        <section class='login'>
+        <form onSubmit="login(event)">
+            <h2>Log In:</h2>
+            <fieldset>
+            <label for="">Email: </label>
+            <input type="text" name="email">
+            </fieldset>
+            <fieldset>
+            <label for="">Password: </label>
+            <input type="password" name="password">
+            </fieldset>
+            <button>Login</button>
+        </form>
+        </section>
+    `;
 }
 
 // Invoked when the user submits the log-in form above
 function login(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  // This is our form DOM object
-  const form = event.target;
+    // This is our form DOM object
+    const form = event.target;
 
-  // Create an object to store all our form input data so that we can send it with our request
-  const data = Object.fromEntries(new FormData(form));
-  
-  // Send a post request to the server along with our form input data
-  fetch('/api/sessions', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  })
-    // Convert the response into JSON. If the response contains an error, show the login page again along with the error. Otherwise, update the state object with the logged-in username and show the character creation page.
-    .then(res => res.json())
-    .then(res => {
-      if (res.error) {
-        renderLogin();
-        renderError(res.error);
-      } else {
-        state.loggedInUserName = res;
-        renderCharacterCreation();
-      }
-    });
+    // Create an object to store all our form input data so that we can send it with our request
+    const data = Object.fromEntries(new FormData(form));
+    
+    // Send a post request to the server along with our form input data
+    fetch('/api/sessions', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+        // Convert the response into JSON. If the response contains an error, show the login page again along with the error. Otherwise, update the state object with the logged-in username and show the character creation page.
+        .then(res => res.json())
+        .then(res => {
+            if (res.error) {
+                renderLogin();
+                renderError(res.error);
+            } else {
+                state.loggedInUserName = res;
+                renderCharacterCreation();
+            }
+        });
 }
 
 function renderError(errorMessage) {
-  document.querySelector('#page').innerHTML = 
-    `<h2 style='color: red;'>${errorMessage}</h2>` + document.querySelector('#page').innerHTML;
+    document.querySelector('#page').innerHTML = 
+        `<h2 style='color: red;'>${errorMessage}</h2>` + document.querySelector('#page').innerHTML;
 }
---
+++ controllers/sessions_controller.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+// Models
+const User = require('../models/user');
+
+// Log the user in if the password matches the password digest
+router.post('/', (req, res) => {
+  const { email, password } = req.body;
+
+  User
+    .findByEmail(email)
+    .then(user => {
+      if (email === '' || password === '') {
+        res.status(400).json({ error: 'email and/or password cannot be blank'});
+      } else {
+        const isValidPassword = bcrypt.compareSync(password, user.password);
+        if (user && isValidPassword) {
+          req.session.userId = user.id;
+          res.json(user.email);
+        }
+      }
+    });
+});
+
+// Log the user in using stored cookie
+router.get('/', (req, res) => {
+  User
+    .findById(req.session.userId)
+    .then(user => {
+      if (user) {
+        res.json(user.email);
+      } else {
+        res.json(null);
+      }
+    })
+});
+
+module.exports = router;
