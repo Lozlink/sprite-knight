fc4673954ad96615d4877f56301afbf4bdc40289
sakalmon <sakalathmon@gmail.com>
2023-01-17 12:37:12 +1000

    added routes and middleware

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 // Bring in express
-const express = require('express')
+const express = require('express');
 
 //--------| Middlewares |--------\\
-const logger = require('./middlewares/logger')
+const logger = require('./middlewares/logger');
+const sessions = require('./middlewares/sessions');
 
 //--------| Controllers |--------\\
+const usersController = require('./controllers/users_controller');
+const sessionsController = require('./controllers/sessions_controller');
+const charactersController = require('./controllers/characters_controller');
 
 // Start up the APP
-const app = express()
+const app = express();
 
 // Initialize the port
-const port = 3000
+const port = 3000;
 
 // Start the web server
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(port, () => console.log(`listening on port ${port}`));
 
 //--------| Express Workflow: Middleware Functions |--------\\
 // Log request information inside the terminal
-app.use(logger)
+app.use(logger);
 // Send back our SPA
-app.use(express.static('client'))
+app.use(express.static('client'));
 // Parses json body assigning the data into req.body
-app.use(express.json())
+app.use(express.json());
+// Enable session so that we can store cookies
+app.use(sessions);
 
 //--------| Middleware for Controllers with Routes|--------\\
+app.use('/api/users', usersController);
+app.use('/api/sessions', sessionsController);
+app.use('/api/characters_controller', charactersController);
